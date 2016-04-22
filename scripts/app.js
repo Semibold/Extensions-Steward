ade2ce8b2021d08a27a3fd157c9a6683944c7ebd
MinonHeart <liuhao.mt@qq.com>
2016-04-22 20:35:39 +0800

    Update: Use Space

--- scripts/app.js
+++ scripts/app.js
@@ -1,94 +1,94 @@
 (function () {
-	
-	var config = {
-		buffer: "chrome.extensions.buffer",
-		status: "chrome.extensions.status"
-	};
-	
-	var status = !!JSON.parse(localStorage.getItem(config.status));
-	var buffer = JSON.parse(localStorage.getItem(config.buffer)) || [];
-	var controller = document.getElementById("controller");
-	
-	function renderer() {
-		chrome.management.getAll(function (result) {
-			var h1 = document.createElement("h1");
-			var ul = document.createElement("ul");
-			
-			h1.innerHTML = chrome.i18n.getMessage(status ? "onekeydisable" : "onekeyenable");
-			h1.addEventListener("click", status ? disableAll : enableAll);
-			h1.setAttribute("data-status", status);
-			
-			result.forEach(function (element) {
-				
-				// 扩展自身和主题不显示在管理列表中
-				if (element.name === chrome.i18n.getMessage("extname") || element.type === "theme") {
-					return;
-				}
-				
-				var li = document.createElement("li");
-				var img = document.createElement("img");
-				var span = document.createElement("span");
-				
-				img.alt = element.name;
-				img.src = element.icons[0].url;
-				span.innerHTML = element.name;
-				li.title = chrome.i18n.getMessage(element.enabled ? "leftclicktodisablethis" : "leftclicktoenablethis");
-				li.id = element.id;
-				li.setAttribute("data-enabled", element.enabled);
-				li.appendChild(img);
-				li.appendChild(span);
-				li.addEventListener("click", changeItem);
-				
-				ul.appendChild(li);
-			});
-			
-			controller.innerHTML = "";
-			controller.appendChild(h1);
-			controller.appendChild(ul);
-		});
-	}
-	
-	
-	
-	function disableAll() {
-		chrome.management.getAll(function (result) {
-			result.forEach(function (element) {
-				if (element.name !== chrome.i18n.getMessage("extname") && element.type !== "theme" && element.enabled) {
-					chrome.management.setEnabled(element.id, false);
-					buffer.push(element.id);
-				}
-			});
-			
-			status = false;
-			localStorage.setItem(config.status, JSON.stringify(status));
-			localStorage.setItem(config.buffer, JSON.stringify(buffer));
-			renderer();
-		});
-	}
-	
-	function enableAll() {
-		buffer.forEach(function (element) {
-			chrome.management.setEnabled(element, true);
-		});
-		
-		status = true;
-		buffer = [];
-		localStorage.setItem(config.status, JSON.stringify(status));
-		localStorage.setItem(config.buffer, JSON.stringify(buffer));
-		renderer();
-	}
-	
-	function changeItem() {
-		chrome.management.get(this.id, function (result) {
-			chrome.management.setEnabled(result.id, !result.enabled, renderer);
-		});
-	}
-	
-	renderer();
-	
+    
+    var config = {
+        buffer: "chrome.extensions.buffer",
+        status: "chrome.extensions.status"
+    };
+    
+    var status = !!JSON.parse(localStorage.getItem(config.status));
+    var buffer = JSON.parse(localStorage.getItem(config.buffer)) || [];
+    var controller = document.getElementById("controller");
+    
+    function renderer() {
+        chrome.management.getAll(function (result) {
+            var h1 = document.createElement("h1");
+            var ul = document.createElement("ul");
+            
+            h1.innerHTML = chrome.i18n.getMessage(status ? "onekeydisable" : "onekeyenable");
+            h1.addEventListener("click", status ? disableAll : enableAll);
+            h1.setAttribute("data-status", status);
+            
+            result.forEach(function (element) {
+                
+                // 扩展自身和主题不显示在管理列表中
+                if (element.name === chrome.i18n.getMessage("extname") || element.type === "theme") {
+                    return;
+                }
+                
+                var li = document.createElement("li");
+                var img = document.createElement("img");
+                var span = document.createElement("span");
+                
+                img.alt = element.name;
+                img.src = element.icons[0].url;
+                span.innerHTML = element.name;
+                li.title = chrome.i18n.getMessage(element.enabled ? "leftclicktodisablethis" : "leftclicktoenablethis");
+                li.id = element.id;
+                li.setAttribute("data-enabled", element.enabled);
+                li.appendChild(img);
+                li.appendChild(span);
+                li.addEventListener("click", changeItem);
+                
+                ul.appendChild(li);
+            });
+            
+            controller.innerHTML = "";
+            controller.appendChild(h1);
+            controller.appendChild(ul);
+        });
+    }
+    
+    
+    
+    function disableAll() {
+        chrome.management.getAll(function (result) {
+            result.forEach(function (element) {
+                if (element.name !== chrome.i18n.getMessage("extname") && element.type !== "theme" && element.enabled) {
+                    chrome.management.setEnabled(element.id, false);
+                    buffer.push(element.id);
+                }
+            });
+            
+            status = false;
+            localStorage.setItem(config.status, JSON.stringify(status));
+            localStorage.setItem(config.buffer, JSON.stringify(buffer));
+            renderer();
+        });
+    }
+    
+    function enableAll() {
+        buffer.forEach(function (element) {
+            chrome.management.setEnabled(element, true);
+        });
+        
+        status = true;
+        buffer = [];
+        localStorage.setItem(config.status, JSON.stringify(status));
+        localStorage.setItem(config.buffer, JSON.stringify(buffer));
+        renderer();
+    }
+    
+    function changeItem() {
+        chrome.management.get(this.id, function (result) {
+            chrome.management.setEnabled(result.id, !result.enabled, renderer);
+        });
+    }
+    
+    renderer();
+    
 })();
 
 
 window.addEventListener("contextmenu", function (e) {
-	e.preventDefault();
+    e.preventDefault();
 });

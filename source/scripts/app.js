6e8d69f475a7a8c55486ffe63eb6dfee46b730a3
MinonHeart <git@hub.moe>
2017-01-05 23:53:31 +0800

    Update: -

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,14 +1,24 @@
 class Steward {
 
     constructor() {
-        this.disabled = false;
-        this.recently = [];
-        this.chromeDisabled = "chrome.disabled";
-        this.chromeRecently = "chrome.recently";
-        this.h1 = document.createElement("h1");
-        this.ul = document.createElement("ul");
-        this.controller = document.getElementById("controller");
-        this.selfID = chrome.i18n.getMessage("@@extension_id");
+        this.state = {
+            disabled: false,
+            recently: [],
+        };
+        this.local = {
+            disabled: "chrome.disabled",
+            recently: "chrome.recently",
+        };
+        this.filter = {
+            id: chrome.i18n.getMessage("@@extension_id"),
+            type: "theme",
+        };
+        this.domNodes = {
+            h1: document.createElement("h1"),
+            ul: document.createElement("ul"),
+            fragment: document.createDocumentFragment(),
+            controller: document.getElementById("controller"),
+        };
         this.decorator();
     }
 
@@ -19,25 +29,25 @@ class Steward {
     }
 
     getLocal() {
-        let disabled = JSON.parse(localStorage.getItem(this.chromeDisabled));
-        let recently = JSON.parse(localStorage.getItem(this.chromeRecently));
+        let disabled = JSON.parse(localStorage.getItem(this.local.disabled));
+        let recently = JSON.parse(localStorage.getItem(this.local.recently));
 
-        this.disabled = Boolean(disabled);
-        this.recently = Array.isArray(recently) ? recently : [];
+        this.state.disabled = Boolean(disabled);
+        this.state.recently = Array.isArray(recently) ? recently : [];
     }
 
     setLocal() {
-        localStorage.setItem(this.chromeDisabled, JSON.stringify(this.disabled));
-        localStorage.setItem(this.chromeRecently, JSON.stringify(this.recently));
+        localStorage.setItem(this.local.disabled, JSON.stringify(this.state.disabled));
+        localStorage.setItem(this.local.recently, JSON.stringify(this.state.recently));
     }
 
     renderer() {
         chrome.management.getAll(result => {
-            let fragment = document.createDocumentFragment();
-            let sortedResult = result.slice().sort((prev, next) => prev.name.localeCompare(next.name, "en-US"));
-
-            sortedResult.forEach(item => {
-                if (item.id !== this.selfID && item.type !== "theme") {
+            result.sort((prev, next) => {
+                return prev.name.localeCompare(next.name, "en-US");
+            }).forEach(item => {
+                console.log(item.name);
+                if (item.id !== this.filter.id && item.type !== this.filter.type) {
                     let li = document.createElement("li");
                     let img = document.createElement("img");
                     let span = document.createElement("span");
@@ -50,24 +60,33 @@ class Steward {
                     li.dataset.enabled = item.enabled;
                     li.appendChild(img);
                     li.appendChild(span);
-                    fragment.appendChild(li);
+                    this.domNodes.fragment.appendChild(li);
                 }
             });
 
-            this.h1.textContent = chrome.i18n.getMessage(this.disabled ? "OneKeyRestore" : "OneKeyDisable");
-            this.ul.textContent = this.controller.textContent = "";
-            this.ul.appendChild(fragment);
-            this.controller.appendChild(this.h1);
-            this.controller.appendChild(this.ul);
+            this.domNodes.h1.textContent = chrome.i18n.getMessage(this.state.disabled ? "OneKeyRestore" : "OneKeyDisable");
+            this.domNodes.ul.textContent = this.domNodes.controller.textContent = "";
+            this.domNodes.ul.appendChild(this.domNodes.fragment);
+            this.domNodes.controller.appendChild(this.domNodes.h1);
+            this.domNodes.controller.appendChild(this.domNodes.ul);
         });
     }
 
     addEvent() {
-        this.h1.addEventListener("click", () => {
-            this.disabled ? this.lastRestore() : this.disableAll();
+        let changeItemStatus = (item, enabled) => {
+            let li = document.querySelector(`li[data-id="${item.id}"]`);
+
+            if (li) {
+                li.title = chrome.i18n.getMessage(enabled ? "DisableItem" : "EnableItem");
+                li.dataset.enabled = enabled;
+            }
+        };
+
+        this.domNodes.h1.addEventListener("click", () => {
+            this.state.disabled ? this.lastRestore() : this.disableAll();
         });
 
-        this.ul.addEventListener("click", e => {
+        this.domNodes.ul.addEventListener("click", e => {
             if (e.target.dataset.id) {
                 chrome.management.get(e.target.dataset.id, item => {
                     chrome.management.setEnabled(item.id, !item.enabled);
@@ -75,8 +94,8 @@ class Steward {
             }
         });
 
-        chrome.management.onEnabled.addListener(item => Steward.changeItemStatus(item, true));
-        chrome.management.onDisabled.addListener(item => Steward.changeItemStatus(item, false));
+        chrome.management.onEnabled.addListener(item => changeItemStatus(item, true));
+        chrome.management.onDisabled.addListener(item => changeItemStatus(item, false));
         chrome.management.onInstalled.addListener(item => this.renderer());
         chrome.management.onUninstalled.addListener(item => this.renderer());
         window.addEventListener("contextmenu", e => e.preventDefault());
@@ -84,49 +103,29 @@ class Steward {
 
     disableAll() {
         chrome.management.getAll(result => {
-            let sortedResult = result.filter(item => {
-                if (item.id !== this.selfID && item.type !== "theme" && item.enabled) {
-                    return item;
+            result.forEach(item => {
+                if (item.id !== this.filter.id && item.type !== this.filter.type && item.enabled) {
+                    chrome.management.setEnabled(item.id, false, () => {
+                        this.state.recently.push(item.id);
+                        this.setLocal();
+                    });
                 }
             });
-            let finalItem = sortedResult[sortedResult.length - 1];
 
-            sortedResult.forEach(item => {
-                chrome.management.setEnabled(item.id, false, () => {
-                    this.recently.push(item.id);
-
-                    if (item.id === finalItem.id) {
-                        this.h1.textContent = chrome.i18n.getMessage("OneKeyRestore");
-                        this.disabled = true;
-                        this.setLocal();
-                    }
-                });
-            });
+            this.domNodes.h1.textContent = chrome.i18n.getMessage("OneKeyRestore");
+            this.state.disabled = true;
         });
     }
 
     lastRestore() {
-        this.recently.forEach((id, index) => {
-            chrome.management.get(id, item => {
-                chrome.management.setEnabled(item.id, true, () => {
-                    if (this.recently.length - 1 === index) {
-                        this.h1.textContent = chrome.i18n.getMessage("OneKeyDisable");
-                        this.disabled = false;
-                        this.recently.length = 0;
-                        this.setLocal();
-                    }
-                });
+        while (this.state.recently.length) {
+            chrome.management.setEnabled(this.state.recently.shift(), true, () => {
+                this.setLocal();
             });
-        });
-    }
-
-    static changeItemStatus(item, enabled) {
-        let li = document.querySelector(`li[data-id="${item.id}"]`);
-
-        if (li) {
-            li.title = chrome.i18n.getMessage(enabled ? "DisableItem" : "EnableItem");
-            li.dataset.enabled = enabled;
         }
+
+        this.domNodes.h1.textContent = chrome.i18n.getMessage("OneKeyDisable");
+        this.state.disabled = false;
     }
 
 }

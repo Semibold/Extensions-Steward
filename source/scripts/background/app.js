7953da841c85d92d0beb290ddf85ec01b4c244e1
L&H <abc@hub.moe>
2018-03-29 21:43:53 +0800

    Fixed: tabs

--- source/scripts/background/app.js
+++ source/scripts/background/app.js
@@ -12,9 +12,7 @@ chrome.omnibox.onInputEntered.addListener((text, disposition) => {
   const url = `https://chrome.google.com/webstore/search/${text}`;
   switch (disposition) {
     case "currentTab":
-      chrome.tabs.query({active: true, highlighted: true,}, tabs => {
-        for (const tab of tabs) chrome.tabs.update(tab.id, {url: url});
-      });
+      chrome.tabs.update({url: url});
       break;
     case "newForegroundTab":
       chrome.tabs.create({url: url, active: true});

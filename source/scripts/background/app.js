03397eb6e9b6b25ff7f466ddedbe26fb48bace53
L&H <abc@hub.moe>
2018-03-29 21:24:05 +0800

    Remove: get more extension

--- source/scripts/background/app.js
+++ source/scripts/background/app.js
@@ -1,17 +1,6 @@
 import {Config} from "../sharre/config.js";
 
 /**
- * @desc 右键菜单：获取更多扩展程序
- */
-chrome.contextMenus.create({
-  title: chrome.i18n.getMessage("get_more_extensions"),
-  contexts: ["browser_action"],
-  onclick: () => chrome.tabs.create({
-    url: "https://chrome.google.com/webstore/category/extensions",
-  }),
-});
-
-/**
  * @desc 右键菜单：卸载扩展
  */
 chrome.contextMenus.create({
